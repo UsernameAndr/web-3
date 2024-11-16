8b7212ae1b9919b996c2415ca1e7a73307722216
Andrew <andrglazkovandr@gmail.com>
2024-11-16 12:35:25 +0300

    Done

--- projects/star/main.go
+++ projects/star/main.go
@@ -1,7 +1,12 @@
 package main
-
 import "fmt"
-
 func main() {
-	fmt.Println("Hello, world!!!")
+	var stroka string
+	fmt.Scan(&stroka)
+	for i, v := range stroka {
+	if i != 0 {
+		fmt.Print("*")
+	}
+		fmt.Printf("%c", v)
+	}
 }
